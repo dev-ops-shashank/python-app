4f91dec6fb89524d5016000b4b082eb65a210cbd
Shashank P <pandeyshashank858@gmail.com>
2025-08-04 20:57:54 +0530

    Adds CD jobs

--- src/app.py
+++ src/app.py
@@ -15,7 +15,7 @@ def details():
     # return '<h1>Hello, World! from Shashank</h1>'
     return jsonify({
         "hostname": socket.gethostname(),
-        'message': 'You are doing great, little human!',
+        'message': 'You are doing great, little human!!',
         "time" :  datetime.now().strftime("%d %B,%Y %H:%M:%S")
         })
 
