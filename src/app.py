96a23f0015626b67e6fcb903d5c0fea51bd2ecef
Shashank P <pandeyshashank858@gmail.com>
2025-08-04 21:45:32 +0530

    Adds new info endpoint

--- src/app.py
+++ src/app.py
@@ -10,13 +10,14 @@ from datetime import datetime
 import socket
 app = Flask(__name__)
 
-@app.route('/api/v1/details')
-def details():
+@app.route('/api/v1/info')
+def info():
     # return '<h1>Hello, World! from Shashank</h1>'
     return jsonify({
         "hostname": socket.gethostname(),
         'message': 'You are doing great, little human [UPDATED]!',
-        "time" :  datetime.now().strftime("%d %B,%Y %H:%M:%S")
+        "time" :  datetime.now().strftime("%d %B,%Y %H:%M:%S"),
+        'deployed_on': 'Kubernetes'
         })
 
 @app.route('/api/v1/healthz')
