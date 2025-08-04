a0418603f218e3b0fb306e79baf2c5befa5c54e4
Shashank P <pandeyshashank858@gmail.com>
2025-08-04 20:13:16 +0530

    Uses self-hosted runners

--- src/app.py
+++ src/app.py
@@ -15,7 +15,7 @@ def details():
     # return '<h1>Hello, World! from Shashank</h1>'
     return jsonify({
         "hostname": socket.gethostname(),
-        'message': 'You are doing great, little human! <3',
+        'message': 'You are doing great, little human!',
         "time" :  datetime.now().strftime("%d %B,%Y %H:%M:%S")
         })
 
